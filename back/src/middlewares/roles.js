f7f28277f4e83f586fbb69881468e33c593c39d9
kryss24 <113718014+kryss24@users.noreply.github.com>
2025-12-05 16:19:40 +0100

    c

--- back/src/middlewares/roles.js
+++ back/src/middlewares/roles.js
@@ -1,5 +1,6 @@
 function roleMiddleware(...allowed) {
   return (req, res, next) => {
+    console.log('Role Middleware - User:', req.user, 'Allowed Roles:', allowed);
     if (!req.user) return res.status(401).json({ message: 'Non authentifié' });
     if (!allowed.includes(req.user.role)) return res.status(403).json({ message: 'Accès refusé' });
     next();
